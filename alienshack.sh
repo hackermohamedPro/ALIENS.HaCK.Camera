8be774c1196df6d317570039f3e91ab0eadacc1c
hackermohamedPro <49755432+hackermohamedPro@users.noreply.github.com>
2019-07-26 19:34:53 +0100

    Update alienshack.sh

--- alienshack.sh
+++ alienshack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # MohamedPro v1.0
-# coded by: github.com/thelinuxchoice/saycheese
+# coded by: MOHAMED LKAMILI
 # If you use any part from this code, giving me the credits. Read the Lincense!
 
 trap 'printf "\n";stop' 2
